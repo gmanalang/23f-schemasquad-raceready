83784ec34b574efef9a4d5c03eb23cd465f9a92e
xukev22 <119141959+xukev22@users.noreply.github.com>
2023-12-04 10:44:48 -0500

    post and get for runner registers route

--- flask-app/src/runners/runners.py
+++ flask-app/src/runners/runners.py
@@ -5,11 +5,12 @@ from src import db
 
 runners = Blueprint('runners', __name__)
 
-# Get all runner registers from the DB
+# Check if runner is registered for certain race
 @runners.route('/runners/<raceID>/<runnerID>', methods=['GET'])
-def get_registers(raceID, runnerID):
+def get_registered(raceID, runnerID):
     cursor = db.get_db().cursor()
-    cursor.execute('select runnerID, raceID from Runner_RegistersFor_Race')
+    query = 'SELECT runnerID, raceID FROM Runner_RegistersFor_Race WHERE raceID = %s AND runnerID = %s'
+    cursor.execute(query, (raceID, runnerID))
     row_headers = [x[0] for x in cursor.description]
     json_data = []
     theData = cursor.fetchall()
@@ -20,17 +21,26 @@ def get_registers(raceID, runnerID):
     the_response.mimetype = 'application/json'
     return the_response
 
-# Get customer detail for customer with particular userID
-@runners.route('/registers/<userID>', methods=['GET'])
-def get_customer(userID):
+@runners.route('/runners', methods=['POST'])
+def register_runner_for_race():
+    
+    # collecting data from the request object 
+    the_data = request.json
+    current_app.logger.info(the_data)
+
+    #extracting the variable
+    raceID = the_data['race_id']
+    runnerID = the_data['runner_id']
+
+    # Constructing the query
+    query = 'insert into Runner_RegistersFor_Race (runnerID, raceID) values ("'
+    query += runnerID + '", "'
+    query += raceID + '")'
+    current_app.logger.info(query)
+
+    # executing and committing the insert statement 
     cursor = db.get_db().cursor()
-    cursor.execute('select * from registers where id = {0}'.format(userID))
-    row_headers = [x[0] for x in cursor.description]
-    json_data = []
-    theData = cursor.fetchall()
-    for row in theData:
-        json_data.append(dict(zip(row_headers, row)))
-    the_response = make_response(jsonify(json_data))
-    the_response.status_code = 200
-    the_response.mimetype = 'application/json'
-    return the_response
+    cursor.execute(query)
+    db.get_db().commit()
+    
+    return 'Success!'

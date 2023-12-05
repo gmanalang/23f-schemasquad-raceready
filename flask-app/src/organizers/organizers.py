18d7965b54db65666fa788eb243a9fcdbc796ee1
gmanalang <gabrielmanalang711@gmail.com>
2023-12-05 16:55:19 -0500

    can now upload race results from the organizer's end

--- flask-app/src/organizers/organizers.py
+++ flask-app/src/organizers/organizers.py
@@ -190,47 +190,72 @@ def delete_race_post(raceID):
 #     json_data.append(dict(zip(column_headers, row)))
 #   return jsonify(json_data)
 
-# # Upload race results of a specific runner
-# @organizers.route('/results/<raceID>', methods=['POST'])
-# def event_organizer_creates_race():
+# Upload race results of a specific runner
+@organizers.route('/results', methods=['POST'])
+def event_organizer_uploads_race_results():
     
-#     # collecting data from the request object 
-#     the_data = request.json
-#     current_app.logger.info(the_data)
+    # collecting data from the request object 
+    the_data = request.json
+    current_app.logger.info(the_data)
 
-#     #extracting the variable
-#     raceName = the_data['race_name']
-#     raceStreet = the_data['street']
-#     raceCity = the_data['city']
-#     raceState = the_data['state']
-#     raceCountry = the_data['country']
-#     raceZip = the_data['zip']
-#     raceDate = the_data['race_date']
-#     terrainType = the_data['terrain_type']
-#     raceLength = the_data['race_length']
-#     maxRunners = the_data['max_runners']
-#     checkInTime = the_data['check_in_time']
-#     organizerID = the_data['organizer_id']
+    #extracting the variable
+    organizerID = the_data['organizer_id']
+    raceID = the_data['race_id']
+    runnerID = the_data['runner_id']
+    bibNumber = the_data['bib_number']
+    finishTime = the_data['finish_time']
+    mileSplit1 = the_data['mile_split_1']
+    mileSplit2 = the_data['mile_split_2']
+    mileSplit3 = the_data['mile_split_3']
+    mileSplit4 = the_data['mile_split_4']
+    mileSplit5 = the_data['mile_split_5']
 
-#     # Constructing the query
-#     query = 'insert into Race (name, street, city, state, country, zip, date, terrainType, raceLength, maxRunners, checkInTime, organizerID) values ("'
-#     query += raceName + '", "'
-#     query += raceStreet + '", "'
-#     query += raceCity + '", "'
-#     query += raceState + '", "'
-#     query += raceCountry + '", "'
-#     query += raceZip + '", "'
-#     query += str(raceDate) + '", "'
-#     query += terrainType + '", "'
-#     query += str(raceLength) + '", "'
-#     query += str(maxRunners) + '", "'
-#     query += str(checkInTime) + '", "'
-#     query += str(organizerID) + '")'
-#     current_app.logger.info(query)
+    # Constructing the first query (inserting into RaceResults table)
+    query = 'INSERT INTO RaceResults (runnerID, bibNumber, finishTime, raceID, organizerID) values ("'
+    query += str(runnerID) + '", "'
+    query += str(bibNumber) + '", "'
+    query += str(finishTime) + '", "'
+    query += str(raceID) + '", "'
+    query += str(organizerID) + '")'
+    current_app.logger.info(query)
 
-#     # executing and committing the insert statement 
-#     cursor = db.get_db().cursor()
-#     cursor.execute(query)
-#     db.get_db().commit()
+    # executing and committing the insert statement 
+    cursor = db.get_db().cursor()
+    cursor.execute(query)
+    db.get_db().commit()
+
+    # Constructing the second query (inserting into MileSplits table)
+    query2 = 'INSERT INTO MileSplits (runnerID, bibNumber, raceID, organizerID, marker, mileSplit) values ("'
+    query2 += str(runnerID) + '", "'
+    query2 += str(bibNumber) + '", "'
+    query2 += str(raceID) + '", "'
+    query2 += str(organizerID) + '", "Mile 5", "'
+    query2 += str(mileSplit1) + '"), ("'
+    query2 += str(runnerID) + '", "'
+    query2 += str(bibNumber) + '", "'
+    query2 += str(raceID) + '", "'
+    query2 += str(organizerID) + '", "Mile 10", "'
+    query2 += str(mileSplit2) + '"), ("'
+    query2 += str(runnerID) + '", "'
+    query2 += str(bibNumber) + '", "'
+    query2 += str(raceID) + '", "'
+    query2 += str(organizerID) + '", "Mile 13.1", "'
+    query2 += str(mileSplit3) + '"), ("'
+    query2 += str(runnerID) + '", "'
+    query2 += str(bibNumber) + '", "'
+    query2 += str(raceID) + '", "'
+    query2 += str(organizerID) + '", "Mile 20", "'
+    query2 += str(mileSplit4) + '"), ("'
+    query2 += str(runnerID) + '", "'
+    query2 += str(bibNumber) + '", "'
+    query2 += str(raceID) + '", "'
+    query2 += str(organizerID) + '", "Mile 25", "'
+    query2 += str(mileSplit5) + '")'
+    current_app.logger.info(query2)
+
+    # executing and committing the insert statement 
+    cursor = db.get_db().cursor()
+    cursor.execute(query2)
+    db.get_db().commit()
     
-#     return 'Success!'
+    return 'Successfuly uploaded race results for this runner!'

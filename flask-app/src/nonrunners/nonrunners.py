918eb59b5b80ad297542f4eab2b915b6926931ce
Siddakk Chatrath <sidda@Sids-MacBook-Pro.local>
2023-12-04 19:54:04 -0500

    added routes

--- flask-app/src/nonrunners/nonrunners.py
+++ flask-app/src/nonrunners/nonrunners.py
@@ -229,7 +229,13 @@ def assign_new_refuel_station():
 @nonrunners.route('/stations/<raceID>/<sponsorID>', methods=['GET'])
 def get_stations(raceID, sponsorID):
     cursor = db.get_db().cursor()
-    cursor.execute('select raceID, sponsorID from Sponsor_Sponsors_Race')
+    query = '''
+        SELECT sponsorName, venueSpot
+        FROM SponsorStation ss JOIN Sponsor s ON ss.sponsorID = s.sponsorID
+                            JOIN Race r ON ss.raceID = r.raceID
+        WHERE ss.raceID = %s AND ss.sponsorID = %s
+    '''
+    cursor.execute(query, (raceID, sponsorID))
     row_headers = [x[0] for x in cursor.description]
     json_data = []
     theData = cursor.fetchall()
@@ -240,15 +246,37 @@ def get_stations(raceID, sponsorID):
     the_response.mimetype = 'application/json'
     return the_response
 
-#How to use raceID here to specify race? Is that even needed?
+#Gets contact info of an event organizer for a race
 @nonrunners.route('/races/<raceID>/<organizerID>', methods=['GET'])
 def get_contact_info(raceID, organizerID):
     cursor = db.get_db().cursor()
     query = '''
-        SELECT name, email, phone
-        FROM EventOrganizer
+        SELECT e.name, e.email, e.phone
+        FROM EventOrganizer e JOIN Race r ON e.organizerID = r.organizerID
+        WHERE e.organizerID = %s AND r.raceID = %s
     '''
-    cursor.execute(query)
+    cursor.execute(query, (organizerID, raceID))
+    row_headers = [x[0] for x in cursor.description]
+    json_data = []
+    theData = cursor.fetchall()
+    for row in theData:
+        json_data.append(dict(zip(row_headers, row)))
+    the_response = make_response(jsonify(json_data))
+    the_response.status_code = 200
+    the_response.mimetype = 'application/json'
+    return the_response
+
+# Gets content of a sponsor's promotional post
+@nonrunners.route('/races/<raceID>/<sponsorID>', methods=['GET'])
+def get_posts(raceID, sponsorID):
+    cursor = db.get_db().cursor()
+    query = '''
+        SELECT s.name, ssr.companyOverview, ssr.websiteLink
+        FROM SponsorStationRace ssr JOIN Sponsor s ON ssr.sponsorID = s.sponsorID
+                                    JOIN Race r ON ssr.raceID = r.raceID
+        WHERE raceID = %s AND sponsorID = %s
+    '''
+    cursor.execute(query, (raceID, sponsorID))
     row_headers = [x[0] for x in cursor.description]
     json_data = []
     theData = cursor.fetchall()
@@ -262,27 +290,40 @@ def get_contact_info(raceID, organizerID):
 # Posting, updating, and deleting a sponsor's promotional post
 @nonrunners.route('/races/<raceID>/<sponsorID>', methods=['POST'])
 def create_post(raceID, sponsorID):
+    the_data = request.json
+    current_app.logger.info(the_data)
+
+    # Extracting the variables
+    overview = the_data['companyOverview']
+    link = the_data['websiteLink']
+    race = the_data['raceID']
+    sponsor = the_data['sponsorID']
+
+    # Constructing the query
+
+    query = 'INSERT INTO SponsorStationRace VALUES ("'
+    query += str(overview) + '", "'
+    query += str(link) + '", "'
+    query += str(race) + '", "'
+    query += str(sponsor) + '")'
+    current_app.logger.info(query)
+
+    # executing and committing the insert statement 
     cursor = db.get_db().cursor()
-    query = '''
-        INSERT INTO Post (title, content)
-        VALUES (?, ?)
-    '''
-    cursor.execute(query, (request.json['title'], request.json['content']))
+    cursor.execute(query)
     db.get_db().commit()
-    the_response = make_response(jsonify({'status': 'success'}))
-    the_response.status_code = 201
-    the_response.mimetype = 'application/json'
-    return the_response
+    
+    return 'Success!'
 
 @nonrunners.route('/races/<raceID>/<sponsorID>', methods=['PUT'])
 def update_post(raceID, sponsorID):
     cursor = db.get_db().cursor()
     query = '''
-        UPDATE Post
-        SET title = ?, content = ?
-        WHERE id = postID
+        UPDATE SponsorStationRace
+        SET companyOverview = %s, websiteLink = %s
+        WHERE raceID = %s AND sponsorID = %s
     '''
-    cursor.execute(query, (request.json['title'], request.json['content'], request.json['id']))
+    cursor.execute(query, (request.json['companyOverview'], request.json['websiteLink'], raceID, sponsorID))
     db.get_db().commit()
     the_response = make_response(jsonify({'status': 'success'}))
     the_response.status_code = 201
@@ -293,12 +334,34 @@ def update_post(raceID, sponsorID):
 def delete_post(raceID, sponsorID):
     cursor = db.get_db().cursor()
     query = '''
-        DELETE FROM Post
-        WHERE id = postID
+        DELETE FROM SponsorStationRace
+        WHERE raceID = %s AND sponsorID = %s
     '''
-    cursor.execute(query, (request.json['id'],))
+    cursor.execute(query, (raceID, sponsorID))
     db.get_db().commit()
     the_response = make_response(jsonify({'status': 'success'}))
     the_response.status_code = 201
+    the_response.mimetype = 'application/json'
+    return the_response
+
+#Check to see if a volunteer is registered for a certain race
+@nonrunners.route('/registrations/<volunteerID>', methods=['GET'])
+def check_registration(volunteerID):
+    cursor = db.get_db().cursor()
+    query = '''
+        SELECT v.firstName, v.lastName
+        FROM Volunteer_RegistersFor_Race
+        JOIN Volunteer AS v
+        ON Volunteer_RegistersFor_Race.volunteerID = v.volunteerID
+        WHERE v.volunteerID = %s
+    '''
+    cursor.execute(query, (volunteerID))
+    row_headers = [x[0] for x in cursor.description]
+    json_data = []
+    theData = cursor.fetchall()
+    for row in theData:
+        json_data.append(dict(zip(row_headers, row)))
+    the_response = make_response(jsonify(json_data))
+    the_response.status_code = 200
     the_response.mimetype = 'application/json'
     return the_response
